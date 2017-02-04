5d7b3236515f48b5b88555998e6cc20d40040e1b
Yodsaphon <hotnoname@hotmail.com>
2017-02-04 16:38:27 +0700

    demo-2

---
+++ demo2017/Assets/Script/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+	public Rigidbody rg;
+	public float speed;
+	public TextMesh txtBall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoJump(){
+		rg.velocity = new Vector3(0,speed,0);
+	}
+	int countCollosion = 0;
+	void OnCollisionEnter(Collision collision) {
+		Debug.LogFormat ("collision is {0}",collision.gameObject.tag);
+		if (collision.gameObject.tag == "floor") {
+			countCollosion++;
+			txtBall.text = "Floor : " + countCollosion.ToString ();
+		} 
+//		else if (collision.gameObject.tag == "ceil") {
+//			Destroy (gameObject);
+//		}
+	}
+}
---
+++ demo2017/Assets/Script/CubeMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+	public Rigidbody rg;
+	public float velocity;
+	// Use this for initialization
+	void Start () {
+		rg.velocity = new Vector3 (velocity, 0, 0);
+
+		//rg.AddForce(new Vector3(-1,0,0));
+		//rg.AddForce(-1,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
